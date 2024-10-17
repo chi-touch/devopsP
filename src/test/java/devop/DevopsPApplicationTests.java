77f4df2b3ff55cc536bd0e01610c70a4f99652f5
chi-touch <gloebudav@gmail.com>
2024-10-17 07:51:24 +0100

    first commit

---
+++ src/test/java/devop/DevopsPApplicationTests.java
@@ -0,0 +1,13 @@
+package devop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
